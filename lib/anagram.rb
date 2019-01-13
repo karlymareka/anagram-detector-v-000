6e5fdf0e538a371d111fa3deb497784e030c29e8
Karly Mareka <karly.mareka@gmail.com>
2019-01-13 18:17:12 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,8 +10,8 @@ class Anagram
   def match(array)
     matches = []
     array.each do |possible_match|
-      possible_match.split(//)
-      word.split(//)
+      possible_match.split(//)!
+      word.split(//)!
         if possible_match.sort == word.sort
           possible_match.join(",")
           matches << possible_match
