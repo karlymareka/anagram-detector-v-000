f2d4daf33e57436ce512cef49ff92c73450895b5
Karly Mareka <karly.mareka@gmail.com>
2019-01-13 18:18:41 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,10 +10,9 @@ class Anagram
   def match(array)
     matches = []
     array.each do |possible_match|
-      possible_match.split(//)!
-      word.split(//)!
-        if possible_match.sort == word.sort
-          possible_match.join(",")
+      match_letters = possible_match.split(//)
+      word_letters = word.split(//)
+        if match_letters.sort == word_letters.sort
           matches << possible_match
         end 
     end
